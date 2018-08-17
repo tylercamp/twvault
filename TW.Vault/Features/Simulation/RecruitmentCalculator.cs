d446fd43d665947a9ab50b4331b39eed47520712
Tyler Camp <tylerc@avi.com>
2018-08-17 15:17:42 -0400

    Begin recruitment calculator, moving BuildingLevels into its own class with BuildingType indexer, begin BuildingStats data

---
+++ TW.Vault/Features/Simulation/RecruitmentCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JSON = TW.Vault.Model.JSON;
+using Native = TW.Vault.Model.Native;
+
+namespace TW.Vault.Features.Simulation
+{
+    public class RecruitmentCalculator
+    {
+        float worldSpeed;
+        int barracksLevel, stablesLevel, workshopLevel;
+
+        public RecruitmentCalculator(float worldSpeed)
+        {
+            this.worldSpeed = worldSpeed;
+        }
+
+        public RecruitmentCalculator(float worldSpeed, int barracksLevel, int stablesLevel, int workshopLevel)
+        {
+            this.worldSpeed = worldSpeed;
+            this.barracksLevel = barracksLevel;
+            this.stablesLevel = stablesLevel;
+            this.workshopLevel = workshopLevel;
+        }
+
+        public RecruitmentCalculator(float worldSpeed, JSON.BuildingLevels buildingLevels)
+        {
+            this.worldSpeed = worldSpeed;
+            this.barracksLevel = buildingLevels.GetValueOrMax(Native.BuildingType.Barracks);
+            this.stablesLevel = buildingLevels.GetValueOrMax(Native.BuildingType.Stables);
+            this.workshopLevel = buildingLevels.GetValueOrMax(Native.BuildingType.Garage);
+        }
+    }
+}
---
+++ TW.Vault/Model/JSON/BuildingLevels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TW.Vault.Model.JSON
+{
+    public class BuildingLevels : Dictionary<String, short>
+    {
+        public short this[Native.BuildingType buildingType]
+        {
+            get => this[buildingType.ToString().ToLower()];
+            set => this[buildingType.ToString().ToLower()] = value;
+        }
+
+        public short GetValueOrDefault(Native.BuildingType buildingType, short defaultValue)
+        {
+            if (this.ContainsKey(buildingType.ToString().ToLower()))
+                return this[buildingType];
+            else
+                return defaultValue;
+        }
+
+        public short GetValueOrMax(Native.BuildingType buildingType)
+        {
+            if (this.ContainsKey(buildingType.ToString().ToLower()))
+                return this[buildingType];
+            else
+                return Native.BuildingStats.MaxLevels[buildingType];
+        }
+    }
+}
